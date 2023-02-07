11e0a28219528faa829d3b89ffea319bf18aa84f
rrrendonr <riky2535@gmail.com>
2023-02-06 21:46:50 -0500

    api expuesta y corregida

--- src/main/java/com/platzi/market/web/controller/PurchaseController.java
+++ src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -29,7 +29,7 @@ public class PurchaseController {
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Purchase> save(Purchase purchase) {
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
     }
 }
